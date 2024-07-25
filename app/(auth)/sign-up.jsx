b377d4294c15724a52a9b6892c4d88d572407c79
lejodev <alejandro.rv97@gmail.com>
2024-07-25 18:19:04 -0500

    Auth started

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,11 +1,19 @@
 import { ScrollView, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import FormField from "../../components/navigation/FormField";
 import CustomButton from "../../components/CustomButton";
 import { Link } from "expo-router";
+import { createUser } from "../../lib/Strapi";
 
 const SignIn = () => {
+  const [form, setForm] = useState({ username: "", email: "", password: "" });
+
+  async function handleSubmit() {
+    console.log("THIS IS THA FORM", form);
+    const user = await createUser(form);
+    console.log(user);
+  }
   return (
     <SafeAreaView className={"bg-primary h-full"}>
       <ScrollView>
@@ -17,18 +25,31 @@ const SignIn = () => {
             title={"Username"}
             placeholder="Type here your username"
             otherStyles="mt-10"
+            handleChangeText={(e) => {
+              setForm({ ...form, username: e });
+            }}
           />
           <FormField
             title={"Email"}
             placeholder="Type here your username"
             otherStyles="mt-10"
+            handleChangeText={(e) => {
+              setForm({ ...form, email: e });
+            }}
           />
           <FormField
             title={"Password"}
             placeholder="Type here your password"
             otherStyles="mt-10"
+            handleChangeText={(e) => {
+              setForm({ ...form, password: e });
+            }}
           />
-          <CustomButton title="SignIn" containerStyles="mt-7" />
+          <CustomButton
+            title="SignIn"
+            containerStyles="mt-7"
+            handlePress={handleSubmit}
+          />
           <View className="justify-center pt-5 flex-row gap-2">
             <Text className="text-lg text-gray-100 font-pregular">
               Already have an account
---
+++ app/config.js
@@ -0,0 +1,3 @@
+export const PROD_URL = process.env.PROD_URL;
+export const PROD_BLOGS_URL = process.env.PROD_BLOGS_URL;
+// export const PUBLIC_API=process.env.
